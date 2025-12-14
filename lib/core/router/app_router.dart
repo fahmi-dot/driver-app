699543d81c5742eea6edde15f04bab498d0da2f2
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-14 20:52:35 +0700

    chore: add new route in app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,16 +1,20 @@
+import 'package:driver_app/features/drive/presentation/screens/camera_action_screen.dart';
 import 'package:driver_app/features/drive/presentation/screens/home_screen.dart';
+import 'package:driver_app/features/drive/presentation/screens/job_detail_screen.dart';
+import 'package:driver_app/features/drive/presentation/screens/photo_preview_screen.dart';
+import 'package:driver_app/features/drive/presentation/screens/stop_action_screen.dart';
 import 'package:driver_app/shared/screens/splash_screen.dart';
 import 'package:go_router/go_router.dart';
-import 'package:uuid/uuid.dart';
 
 class Routes {
   static const splash = '/';
   static const home = '/home';
   static const jobDetail = '/job/:id';
-  static const cameraAction = '/camera';
   static const stopAction = '/stop';
+  static const cameraAction = '/camera';
+  static const photoPreview = '/photo';
 
-  static String jobWithId(Uuid id) => '/job/$id';
+  static String jobWithId(String id) => '/job/$id';
 }
 
 final appRouter = GoRouter(
@@ -18,11 +22,39 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: Routes.splash,
-      builder: (context, state) => const SplashScreen(),
+      builder: (context, state) => SplashScreen(),
     ),
     GoRoute(
       path: Routes.home,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => HomeScreen(),
+    ),
+    GoRoute(
+      path: Routes.jobDetail,
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        return JobDetailScreen(id: id);
+      },
+    ),
+    GoRoute(
+      path: Routes.stopAction,
+      builder: (context, state) {
+        final extra = state.extra as Map<String, dynamic>;
+        return StopActionScreen(
+          jobId: extra['jobId'], 
+          stop: extra['stop'],
+        );
+      },
+    ),
+    GoRoute(
+      path: Routes.cameraAction,
+      builder: (context, state) => CameraActionScreen(),
+    ),
+    GoRoute(
+      path: Routes.photoPreview,
+      builder: (context, state) {
+        final extra = state.extra as Map<String, dynamic>;
+        return PhotoPreviewScreen(mediaPath: extra['mediaPath']);
+      },
     ),
   ]
 );
