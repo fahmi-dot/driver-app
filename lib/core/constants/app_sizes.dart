7c95a22a7dcdcf88faf47d172e84cfbc8ec4100c
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-14 09:01:10 +0700

    chore: add constant files

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,30 @@
+class AppSizes {
+  static const double paddingXS = 4.0;
+  static const double paddingS = 8.0;
+  static const double paddingM = 12.0;
+  static const double paddingL = 16.0;
+  static const double paddingXL = 20.0;
+  static const double padding2XL = 24.0;
+
+  static const double fontXS = 10.0;
+  static const double fontS = 11.0;
+  static const double fontM = 12.0;
+  static const double fontL = 14.0;
+  static const double fontXL = 18.0;
+  static const double font2XL = 24.0;
+
+  static const double iconXS = 4.0;
+  static const double iconS = 8.0;
+  static const double iconM = 12.0;
+  static const double iconL = 16.0;
+  static const double iconXL = 20.0;
+  static const double icon2XL = 24.0;
+  static const double iconMain = 80.0;
+
+  static const double radiusXS = 4.0;
+  static const double radiusS = 8.0;
+  static const double radiusM = 12.0;
+  static const double radiusL = 16.0;
+  static const double radiusXL = 20.0;
+  static const double radius2XL = 24.0;
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,27 @@
+class AppStrings {
+  static const String appName = 'UDrive';
+
+  static const String pending = 'Tertunda';
+  static const String ongoing = 'Berlangsung';
+  static const String completed = 'Selesai';
+
+  static const String createdAt = 'Dibuat pada';
+  static const String startedAt = 'Dimulai pada';
+  static const String completedAt = 'Selesai pada';
+
+  static const String noPendingJobs = 'Belum ada pekerjaan tertunda';
+  static const String noOngoingJobs = 'Belum ada pekerjaan berlangsung';
+  static const String noCompletedJobs = 'Belum ada pekerjaan selesai';
+
+  static const String startJobAsk = 'Apakah Anda yakin ingin memulai pekerjaan ini?';
+
+  static const String jobStartedNotif = 'Pekerjaan dimulai!';
+
+  static const String stop = 'Stop';
+  static const String stopList = 'Daftar Stop';
+  static const String details = 'Detail';
+
+  static const String startJob = 'Mulai pekerjaan';
+  static const String cancel = 'Batal';
+  static const String start = 'Mulai';
+}
