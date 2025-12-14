da826c6a7489ff79547ae24fa4cf49ee8c9cacf0
fahmi-dot <agilfahmirakasaputra073@gmail.com>
2025-12-14 21:05:18 +0700

    feat: add user permissions for camera and location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:driver_app/app.dart';
+import 'package:driver_app/core/utils/permissions.dart';
 import 'package:driver_app/shared/providers/shared_preferences_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,6 +8,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Permissions.requestCamera();
+  await Permissions.requestLocation();
   final prefs = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
